43c10495f85a4cb2c1521dd0ca99da2e446103e3
parathwaajk <90028601+parathwaajk@users.noreply.github.com>
2021-09-06 20:44:00 +0700

    Delete cricket batter and score.py

--- cricket batter and score.py
+++
@@ -1,7 +0,0 @@
-batter=["Rohit","KL rahul","Pujara","Jadeja","Kholi"]
-score=[23,45,33,65,12]
-print("The "+ str(batter[0]) ,"batting score "+ str (score[0]))
-print("The "+ str(batter[1]) ,"batting score "+ str (score[1]))
-print("The "+ str(batter[2]) ,"batting score "+ str (score[2]))
-print("The "+ str(batter[3]) ,"batting score "+ str (score[3]))
-print("The "+ str(batter[4]) ,"batting score "+ str (score[4]))
